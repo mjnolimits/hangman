ebd8fc95ec8329aafe72cff3f92c9f37575d3b24
“mjnolimits” <nosxbject@gmail.com>
2023-10-30 11:12:35 +0000

    commit

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,35 +1,35 @@
-# milestone_3.py
-
 import random
 
-# List of favorite fruits
-favorite_fruits = ['apple', 'banana', 'orange', 'strawberry', 'kiwi']
+# Create a list of 5 favorite fruits
+favorite_fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+word_list = favorite_fruits
 
-# Randomly choose a word from the list
-word = random.choice(favorite_fruits)
+# Use random.choice to select a random word from word_list
+word = random.choice(word_list)
 
-# Function to check if the guessed letter is in the word
 def check_guess(guess):
+    # Step 2: Convert the guess into lowercase
     guess = guess.lower()
-    if guess in word.lower():
-        print(f"Good guess! {guess} is in the word.")
-        return True
+
+    # Step 3: Check if the guess is in the word
+    if guess in word:
+        # If the guess is in the word, print "Good guess!"
+        print(f"Good guess! '{guess}' is in the word.")
     else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
-        return False
+        # If the guess is not in the word, print an error message
+        print(f"Sorry, '{guess}' is not in the word. Try again.")
 
-# Function to ask the user for input and validate the guess
 def ask_for_input():
+    # Step 1: Define a function called ask_for_input
     while True:
+        # Step 2: Move the input validation code here
         guess = input("Guess a letter: ")
-
-        # Check if the guess is a single alphabetical character
-        if len(guess) == 1 and guess.isalpha():
-            if check_guess(guess):
-                break
+        if guess.isalpha() and len(guess) == 1:
+            # Step 3: Call the check_guess function to check if the guess is in the word
+            check_guess(guess)
         else:
+            # If the guess doesn't pass the checks, print an error message and continue to prompt for a valid guess
             print("Invalid letter. Please, enter a single alphabetical character.")
 
-# Call the function to ask for input and check the guess
+# Step 4: Call the ask_for_input function to test your code
 ask_for_input()
-print("The secret word was:", word)
---
+++ hangman/milestone_4.py
@@ -0,0 +1,42 @@
+import random
+
+class Hangman:
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess = guess.lower()  # Convert the guessed letter to lowercase
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+            for i in range(len(self.word)):
+                if self.word[i] == guess:
+                    self.word_guessed[i] = guess  # Replace "_" with the guess
+                    self.num_letters -= 1  # Reduce the count of unique letters
+        else:
+            self.num_lives -= 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while self.num_lives > 0 and self.num_letters > 0:
+            guess = input("Guess a letter: ").lower()
+            if len(guess) != 1 or not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)  # Call the check_guess method
+                self.list_of_guesses.append(guess)  # Append the guess to list_of_guesses
+                print(f"Word to guess: {' '.join(self.word_guessed)}")
+                print(f"Number of unique letters: {self.num_letters}")
+
+        if self.num_letters == 0:
+            print("Congratulations! You've guessed the word.")
+        else:
+            print(f"Game over. The word was: {self.word}")
+
